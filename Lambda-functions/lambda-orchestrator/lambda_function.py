e6f16fc49b3e85912935320aa50ad1eaf4b8d3f6
Alberto200420 <105571383+Alberto200420@users.noreply.github.com>
2025-01-26 11:42:56 -0600

    Added security blocking foreign domains

--- Lambda-functions/lambda-orchestrator/lambda_function.py
+++ Lambda-functions/lambda-orchestrator/lambda_function.py
@@ -1,16 +1,34 @@
 from orchestrator import graph, HumanMessage
-import json
 import unicodedata
+import json
+import re
 
 def normalize_text(text) -> str:
-  return unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('ascii')
+  # Remove markdown formatting
+  text = re.sub(r'\*{1,2}', '', text)
+  # Normalize unicode characters
+  text = unicodedata.normalize('NFKD', text)
+  # Remove non-printable characters
+  text = re.sub(r'[^\x20-\x7E\n]', '', text)
+  # Replace multiple newlines with a single newline
+  text = re.sub(r'\n{2,}', '\n', text)
+  # Remove extra whitespace
+  text = re.sub(r'\s+', ' ', text).strip()
+  return text
 
 def lambda_handler(event, context):
+  # Check the Referer header to ensure the request is from the allowed domain
+  referer = event.get('headers', {}).get('referer', '')
+  if referer != 'https://coxcoxai.com/':
+    return {
+      'statusCode': 403,
+      'body': json.dumps({'error': 'Forbidden: Invalid Referer'})
+    }
   try:
     # Parse the message body
     request_body = json.loads(event['body'])
-    text = request_body['text']
-    thread_id = request_body['thread_id']
+    text: str = request_body['text']
+    thread_id: str = request_body['thread_id']
     # Invoke the graph
     result = graph.invoke({"messages": [HumanMessage(content=text)]}, {"configurable": {"thread_id": thread_id}})
     graph_response = normalize_text(result['messages'][-1].content)
